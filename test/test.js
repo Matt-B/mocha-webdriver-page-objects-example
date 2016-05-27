8e83d6c26a303629d0cc189d2d054c9195c5fe4c
Matt Bailey <matt.bailey@axonvibe.com>
2016-05-27 10:30:23 +0100

    Add 'done' callbacks to test steps and setup/teardown.

--- test/test.js
+++ test/test.js
@@ -9,22 +9,25 @@ test.describe('Google Search', function() {
   
   this.timeout(15000);
 
-  test.before(function() {
+  test.before(function(done) {
     driver = require('./driver').getDriver();
+    done();
   });
   
-  test.after(function() {
+  test.after(function(done) {
     if(driver)
       driver.quit();
+    done();
   }); 
 
-  test.it('should display results when searching for dinosaurs', function() {
+  test.it('should display results when searching for dinosaurs', function(done) {
     var googleSearchPage = new GoogleSearchPage(driver);
     googleSearchPage.open();
     googleSearchPage.typeSearchQuery('dinosaurs');
     var googleResultsPage = googleSearchPage.clickSearchButton();
     googleResultsPage.getDisplayedResults().then(function(numberOfResults) {
       expect(numberOfResults.length).to.be.above(0);
+      done();
     });
   });
 
